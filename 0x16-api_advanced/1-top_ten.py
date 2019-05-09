6354fa42925f1a49ed390e1b012f78ca9da68b1d
sazad44 <530@holbertonschool.com>
2019-05-09 20:09:36 +0000

    Updating task 1 py file with correct subreddit validation check

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,15 +5,22 @@ import requests
 
 def top_ten(subreddit):
     """top_ten method to get the top ten hot articles on a subreddit"""
-    respAbout = requests.get('https://reddit.com/r/{}/about.json'
-                             .format(subreddit),
-                             headers={'User-Agent': 'Safari 12.1'})
-    response = requests.get("https://reddit.com/r/{}.json"
-                            .format(subreddit),
-                            headers={'User-Agent': 'Safari 12.1'})
-    if respAbout.json().get('data').get('subscribers') is None:
+    subRcheck = requests.get('https://reddit.com/api/search_reddit_names.json',
+                             headers={
+                                 'User-Agent': 'Safari 12.1'
+                             },
+                             params={
+                                 'exact': True,
+                                 'query': subreddit
+                             })
+    if 'error' in subRcheck.json().keys():
         print(None)
     else:
+        response = requests.get("https://reddit.com/r/{}.json"
+                                .format(subreddit),
+                                headers={
+                                    'User-Agent': 'Safari 12.1'
+                                })
         printRange = 10
         if len(response.json().get('data').get('children')) < 10:
             printRange = len(response.json().get('data').get('children'))
