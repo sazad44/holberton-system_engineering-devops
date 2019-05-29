03f7530238793ea9564f4e3bc1b29451a7e2e2ed
sazad44 <530@holbertonschool.com>
2019-05-29 17:46:47 +0000

    Updating task 0 and 1 to be puppet-lint compliant

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # Fixing NGINX to accept more files
 exec { 'Fix NGINX Failed Requests':
-     command => "/bin/echo ULIMIT='-n 5000' | sudo tee /etc/default/nginx && sudo service nginx restart"
+  command => "/bin/echo ULIMIT='-n 5000' | sudo tee /etc/default/nginx && sudo service nginx restart"
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # Providing fix for holberton user having too many files
 exec { 'Fix holberton user too many files open':
-     command => "/bin/sed -i 's/holberton.*//g' /etc/security/limits.conf"
+  command => "/bin/sed -i 's/holberton.*//g' /etc/security/limits.conf"
 }
