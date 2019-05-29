ea60ccad957987fc71d2edb469735931ce817e73
sazad44 <530@holbertonschool.com>
2019-05-29 15:44:42 +0000

    Uploading README file and task 0 file to increase the file limit of nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,4 @@
+# Fixing NGINX to accept more files
+exec { 'Fix NGINX Failed Requests':
+     command => "/bin/echo ULIMIT='-n 5000' | sudo tee /etc/default/nginx"
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+# 0x1B Web Stack Debugging #4
