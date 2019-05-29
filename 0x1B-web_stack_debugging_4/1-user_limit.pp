bece9d76aff660d88500f9e98590c3f58df0f293
sazad44 <530@holbertonschool.com>
2019-05-29 17:41:39 +0000

    Uploading task 1 puppet file with error fixes

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
 # Providing fix for holberton user having too many files
 exec { 'Fix holberton user too many files open':
-     command => "sed -i 's/holberton.*//g"
+     command => "/bin/sed -i 's/holberton.*//g' /etc/security/limits.conf"
 }
